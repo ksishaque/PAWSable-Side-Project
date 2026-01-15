117096fd9f2d2a112cd5b7e1ead1ded22ac33bd8
silgif <benedict.yau.sh@gmail.com>
2026-01-14 23:22:04 -0800

    Added Health and Damage
    - Changed CollisionHandler.cs into Health.cs
    - Changed LootHandler.cs into LootCollisionHandler.cs
    - Implemented health for enemies (damage towards enemies is currently fixed)
    - Implemented a limit for how many times a bullet can damage an enemy.

---
+++ BulletGame/Assets/Scripts/Projectiles/DamageHandler.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour
+{
+    [SerializeField] private float pierce = 1;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Player Bullet && Enemy Collision 
+        Debug.Log("Collision Detected");
+
+        if (pierce > 0)
+        {
+            if (collider.gameObject.layer == 8)
+            {
+                pierce -= 1;
+            }
+        }
+
+        if (pierce <= 0)
+        {
+            ObjectDestroyer.destroy(gameObject, ObjectDestroyer.Type.DESPAWN);
+        }
+
+    }
+}
