334957a5400ef6aa1b6a955359c0e11fcc5187d7
silgif <benedict.yau.sh@gmail.com>
2026-02-25 13:28:57 -0800

    Inventory Blocks work
    Can move around Items(currently only an image) around inventory grid.

---
+++ BulletGame/Assets/Scripts/UI/InventoryItem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("UI")]
+    public Image image;
+
+    [HideInInspector] public Transform parentAfterDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = false;
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 mouseCurrentPos = Mouse.current.position.ReadValue();
+        transform.position = new Vector3(mouseCurrentPos.x, mouseCurrentPos.y, 0);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = true;
+        transform.SetParent(parentAfterDrag);
+        transform.localPosition = new Vector3();
+
+    }
+
+}
