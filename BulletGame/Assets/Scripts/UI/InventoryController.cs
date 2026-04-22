90587780f7103f76523c2b679c2dc4ca41aaf4c4
silgif <benedict.yau.sh@gmail.com>
2026-04-21 18:20:43 -0700

    Inventory Drag and Drop works
    Can move 1x1 tile items on the grid.

--- BulletGame/Assets/Scripts/UI/InventoryController.cs
+++ BulletGame/Assets/Scripts/UI/InventoryController.cs
@@ -1,21 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InventoryController : MonoBehaviour
 {
-    public InventorySlot selectedInventorySlot;
 
+    [HideInInspector] public InventorySlot selectedInventorySlot;
+
+    InventoryItem selectedItem;
+    RectTransform rectTransform;
 
     private void Update()
     {
+        if (selectedItem != null)
+        {
+            rectTransform.position = Mouse.current.position.ReadValue(); ;
+        }
         //Debug.Log("InventoryController is Updating");
         if (selectedInventorySlot == null) {
             //Debug.Log("InventoryController doesn't have a Selected Inventory Slot");
             return; 
         }
         //Debug.Log("Inventory Controller is working");
-        Debug.Log(selectedInventorySlot.GetTileGridPosition(Mouse.current.position.ReadValue()));
+
+       
+
+        if ((Mouse.current.leftButton.wasPressedThisFrame || Mouse.current.leftButton.wasReleasedThisFrame) || (Gamepad.current != null && Gamepad.current.startButton.wasPressedThisFrame))
+        {
+            Vector2Int tileGridPosition = selectedInventorySlot.GetTileGridPosition(Mouse.current.position.ReadValue());
+            
+            if(selectedItem == null)
+            {
+                selectedItem = selectedInventorySlot.PickUpItem(tileGridPosition.x, tileGridPosition.y);
+                if (selectedItem != null)
+                {
+                    rectTransform = selectedItem.GetComponent<RectTransform>();
+                }
+                
+            }
+            else
+            {
+                selectedInventorySlot.PlaceItem(selectedItem, tileGridPosition.x, tileGridPosition.y);
+                selectedItem = null;
+            }
+        }
+
     }
 }
